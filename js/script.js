68c20fbd6aa1cfb7d3fbd8d8da0225fa175f4647
Dipenkoradiya <dipentj45@gmail.com>
2024-02-13 17:08:33 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const btnnavel = document.querySelector(".nav-menu-button");
+const headerel = document.querySelector(".header-main");
+btnnavel.addEventListener("click", function () {
+    headerel.classList.toggle("nav-open");
+});
+console.log("hello");
